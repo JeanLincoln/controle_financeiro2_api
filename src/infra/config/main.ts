013968441ce0b0824a6888a33fca90c192117438
Jean Lincoln <jeanlincol@gmail.com>
2025-05-26 07:47:43 -0300

    CF2-005: Update package.json and pnpm-lock.yaml to add @nestjs/swagger and docker command; configure Swagger in main.ts

--- src/infra/config/main.ts
+++ src/infra/config/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "../modules/app";
+import { SwaggerConfig } from "./swagger";
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
+  SwaggerConfig.config(app);
 
   await app.listen(process.env.PORT!);
 
---
+++ src/infra/config/swagger/index.ts
@@ -0,0 +1,14 @@
+import type { INestApplication } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+
+export class SwaggerConfig {
+  static config(app: INestApplication) {
+    const config = new DocumentBuilder()
+      .setTitle("Controle Financeiro 2")
+      .setDescription("Um app para gerenciar suas finanças")
+      .setVersion("0.0.1")
+      .build();
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("api", app, documentFactory);
+  }
+}
